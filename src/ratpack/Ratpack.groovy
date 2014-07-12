b31b94844c22d284c2ea4399d1d59cea3c93e72f
Roberto Guerra <uris77@gmail.com>
2014-07-12 10:50:56 -0600

    Using custom domain

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -15,7 +15,7 @@ ratpack {
         add new MapSessionsModule(10, 5)
         bind Pac4jCallbackHandler
         GoogleOpenIdClient openIdClient = new GoogleOpenIdClient()
-        openIdClient.callbackUrl = "http://aqueous-ravine-3371.herokuapp.com/pac4j-callback"
+        openIdClient.callbackUrl = "http://ratpack-oauth.stumglincode.com/pac4j-callback"
         add new Pac4jModule(openIdClient, new AuthPathAuthorizer())
     }
 
